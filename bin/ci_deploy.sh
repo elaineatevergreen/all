421e9f46040f582a26fb37a75009900c6fcdeb51
David Metzler <metzlerd@evergreen.edu>
2016-02-01 09:14:09 -0800

    Adding library deployment.

--- bin/ci_deploy.sh
+++ bin/ci_deploy.sh
@@ -7,6 +7,10 @@ deploy_d7_theme themes/wwwevergreen $COLLAB_CODE/drupal7
 deploy_d7_module modules/custom/learning_community_directory $COLLAB_CODE/drupal7
 # The new www7 sites are not on collab yet.
 if [ "$STAGE"=='dev' ] ; then
+  # Required Libraries
+  deploy_d7_library libraries/iCalcreator
+  deploy_d7_library libraries/plupload
+  # Feature modules
   deploy_d7_theme themes/wwwevergreen $COLLAB_CODE/www7
   deploy_d7_custom_module modules/custom/evergreen_content $COLLAB_CODE/www7
   deploy_d7_custom_module modules/custom/campus_calendar $COLLAB_CODE/www7
