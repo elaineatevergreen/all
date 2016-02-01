d9537fb2aadbad2f2ff608abc681d05bbbb6beb8
David Metzler <metzlerd@evergreen.edu>
2016-02-01 09:03:03 -0800

    Removed deployment of campus calendar to collab general code tree.

--- bin/ci_deploy.sh
+++ bin/ci_deploy.sh
@@ -7,7 +7,6 @@ deploy_d7_theme themes/wwwevergreen $COLLAB_CODE/drupal7
 deploy_d7_module modules/custom/learning_community_directory $COLLAB_CODE/drupal7
 # The new www7 sites are not on collab yet.
 if [ "$STAGE"=='dev' ] ; then
-  deploy_d7_module modules/custom/campus_calendar $COLLAB_CODE/drupal7
   deploy_d7_theme themes/wwwevergreen $COLLAB_CODE/www7
   deploy_d7_custom_module modules/custom/evergreen_content $COLLAB_CODE/www7
   deploy_d7_custom_module modules/custom/campus_calendar $COLLAB_CODE/www7
