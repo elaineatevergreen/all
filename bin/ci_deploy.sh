c890e5dd5e6ab30f79a89e2c36f2f57f4078e28e
David Metzler <metzlerd@evergreen.edu>
2016-02-01 09:18:06 -0800

    Woops correction.

--- bin/ci_deploy.sh
+++ bin/ci_deploy.sh
@@ -8,8 +8,8 @@ deploy_d7_module modules/custom/learning_community_directory $COLLAB_CODE/drupal
 # The new www7 sites are not on collab yet.
 if [ "$STAGE"=='dev' ] ; then
   # Required Libraries
-  deploy_d7_library libraries/iCalcreator
-  deploy_d7_library libraries/plupload
+  deploy_d7_library libraries/iCalcreator $COLLAB_CODE/www7
+  deploy_d7_library libraries/plupload $COLLAB_CODE/www7
   # Feature modules
   deploy_d7_theme themes/wwwevergreen $COLLAB_CODE/www7
   deploy_d7_custom_module modules/custom/evergreen_content $COLLAB_CODE/www7
