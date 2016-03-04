e4d7e5d01b172bd5886b0461b51effce6fd49a2b
David Metzler <metzlerd@evergreen.edu>
2016-03-04 13:16:14 -0800

    Make sure file_enitty module gets enabled before media module update is attempted.

--- site_updates_collab/rev.2016-02-002.new-modules.sh
+++ site_updates_collab/rev.2016-02-002.new-modules.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 drush dl --yes file_entity-7.x-2.0-beta2
-drush @sites updb
+drush collab${SITE_SUFFIX}.evergreen.edu.cms en file_entity
+drush collab${SITE_SUFFIX}.evergreen.edu.policies en file_entity
 drush dl --yes media-7.x-2.0-beta1
 drush --yes @sites updb
