4ada2acf59df2d1b88223e226e3e40e6e41e89bf
Elaine Nelson <me@elainenelson.org>
2016-03-04 15:08:50 -0800

    Revert "Roll back, reimport SiteSection/Content migrations"
    This reverts commit 80f4d6823a58e1712642f97d0b0e65aa75604442.

--- site_updates_www/rev.2016-02-003.new-modules.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-drush --yes collab${SITE_SUFFIX}.evergreen.edu.www mr SiteSections
-drush --yes collab${SITE_SUFFIX}.evergreen.edu.www mr SiteContent
-drush --yes collab${SITE_SUFFIX}.evergreen.edu.www mi SiteSections
-drush --yes collab${SITE_SUFFIX}.evergreen.edu.www mi SiteContent
