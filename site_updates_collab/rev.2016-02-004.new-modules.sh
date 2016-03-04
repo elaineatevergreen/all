3042af2d7c31f02a0b85809e676daeae70e689db
David Metzler <metzlerd@evergreen.edu>
2016-03-04 13:35:42 -0800

    Change so this only gets attempted once.

--- site_updates_collab/rev.2016-02-004.new-modules.sh
+++ site_updates_collab/rev.2016-02-004.new-modules.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 drush dl --yes file_entity-7.x-2.0-beta2
-drush dl --yes media_ckeditor-7.x-2.0-alpha1
+drush --yes collab${SITE_SUFFIX}.evergreen.edu.cms en file_entity
+drush --yes collab${SITE_SUFFIX}.evergreen.edu.policies en file_entity
+drush dl --yes media-7.x-2.0-beta1
 drush --yes @sites updb
