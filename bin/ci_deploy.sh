9bab22b007361bb3c54125948388a741ec2b59fc
David Metzler <metzlerd@evergreen.edu>
2016-03-28 15:59:34 -0700

    Allow deployment of calendar/www sites to production.

--- bin/ci_deploy.sh
+++ bin/ci_deploy.sh
@@ -7,14 +7,12 @@
 deploy_d7_theme themes/wwwevergreen $COLLAB_CODE/drupal7
 deploy_d7_module modules/custom/learning_community_directory $COLLAB_CODE/drupal7
 run_site_updates $COLLAB_CODE/drupal7 site_updates_collab
-# The new www7 sites are not on collab yet.
-if [ "$STAGE"=='dev' ] ; then
-  # Required Libraries
-  deploy_d7_library libraries/iCalcreator $COLLAB_CODE/www7
-  deploy_d7_library libraries/plupload $COLLAB_CODE/www7
-  # Feature modules
-  deploy_d7_theme themes/wwwevergreen $COLLAB_CODE/www7
-  deploy_d7_custom_module modules/custom/evergreen_content $COLLAB_CODE/www7
-  deploy_d7_custom_module modules/custom/campus_calendar $COLLAB_CODE/www7
-  run_site_updates $COLLAB_CODE/www7 site_updates_www
-fi
+# www deployment.
+# Required Libraries
+deploy_d7_library libraries/iCalcreator $COLLAB_CODE/www7
+deploy_d7_library libraries/plupload $COLLAB_CODE/www7
+# Feature modules
+deploy_d7_theme themes/wwwevergreen $COLLAB_CODE/www7
+deploy_d7_custom_module modules/custom/evergreen_content $COLLAB_CODE/www7
+deploy_d7_custom_module modules/custom/campus_calendar $COLLAB_CODE/www7
+run_site_updates $COLLAB_CODE/www7 site_updates_www
