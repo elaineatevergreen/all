e8c54bb8ad8506a3a7b74bec59be4f8a7a997697
Elaine Nelson <me@elainenelson.org>
2016-05-09 11:16:58 -0700

    Customizes views summary template for directories

---
+++ themes/wwwevergreen/templates/views-view-summary--directory.tpl.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @file
+ * Default simple view template to display a list of summary lines.
+ *
+ * @ingroup views_templates
+ 
+ this version uses the Evergreen "alpha-index" style
+ */
+?>
+
+
+  <ul class="views-summary alpha-index">
+  <?php foreach ($rows as $id => $row): ?>
+    <li><a href="<?php print $row->url; ?>"<?php print !empty($row_classes[$id]) ? ' class="'. $row_classes[$id] .'"' : ''; ?>><?php print $row->link; ?></a>
+      <?php if (!empty($options['count'])): ?>
+        (<?php print $row->count?>)
+      <?php endif; ?>
+    </li>
+  <?php endforeach; ?>
+  </ul>
+
