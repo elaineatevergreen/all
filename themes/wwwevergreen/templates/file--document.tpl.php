5e42635f88d8d29658d3e47871a6c51e38774356
Elaine Nelson <me@elainenelson.org>
2016-08-23 11:22:46 -0700

    Strips commented out markup from file template

--- themes/wwwevergreen/templates/file--document.tpl.php
+++ themes/wwwevergreen/templates/file--document.tpl.php
@@ -68,28 +68,13 @@
  * @ingroup themeable
  */
 ?>
-<!--<div id="<?php print $id; ?>" class="<?php print $classes ?>"<?php print $attributes; ?>>
-
-  <?php print render($title_prefix); ?>
-  <?php if (!$page): ?>
-    <h2<?php print $title_attributes; ?>><a href="<?php print $file_url; ?>"><?php print $label; ?></a></h2>
-  <?php endif; ?>
-  <?php print render($title_suffix); ?>
-
-  <?php if ($display_submitted): ?>
-    <div class="submitted">
-      <?php print $submitted; ?>
-    </div>
-  <?php endif; ?>
-
-  <div class="content"<?php print $content_attributes; ?>>-->
     <?php
       // We hide the links now so that we can render them later.
-      hide($content['links']);
-      print render($content);
+      //hide($content['links']);
+      //print render($content);
     ?>
-  <!--</div>-->
+
 
   <?php print render($content['links']); ?>
 
-<!--</div>-->
+
