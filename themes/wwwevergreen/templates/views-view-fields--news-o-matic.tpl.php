a9dabca23527f55835473fc18036930c36387a87
Elaine Nelson <me@elainenelson.org>
2016-10-04 12:35:45 -0700

    Updates to theme for news feed view

--- themes/wwwevergreen/templates/views-view-fields--news-o-matic.tpl.php
+++ themes/wwwevergreen/templates/views-view-fields--news-o-matic.tpl.php
@@ -6,16 +6,18 @@ Formatting for individual items in the news-o-matic view, as seen on the home pa
 ?>
 <article class="news-article">
 <header>
-	<h3><?php print $fields['title']->content ?></h3>
+	<h3 class="lead"><?php print $fields['title']->content ?></h3>
 </header>
+<div class="news-body">
 <p>
 	<q><?php print $fields['field_description_plain']->content ?></q>
 </p>
+</div>
 <footer>
 	<p>
 		<cite><?php print $fields['field_article_link']->content ?></cite>
 	</p>
-	<p>
+	<p class="source">
 		(<i class="source"><?php print $fields['field_publication_title']->content ?></i>,
 		<?php print $fields['field_article_date']->content ?>)
 	</p>
