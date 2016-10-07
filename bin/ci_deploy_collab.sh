8c914fa7d9762548542faf76cd2a483dd7224a15
David Metzler <metzlerd@evergreen.edu>
2016-10-07 13:49:53 -0700

    Full speration of www and collab.

--- bin/ci_deploy_collab.sh
+++ bin/ci_deploy_collab.sh
@@ -13,16 +13,4 @@ deploy_d7_module modules/custom/evergreen_cas $COLLAB_CODE/drupal7
 echo "Collab Site updates"
 run_site_updates $COLLAB_CODE/drupal7 site_updates_collab
 # www deployment.
-echo "Beginning WWW Deployment"
-if [ -f $WWW_CODE/index.php ] && [[ "$STAGE" != "dev" ]] ; then
-  # Required Libraries
-  deploy_d7_library libraries/iCalcreator $WWW_CODE
-  deploy_d7_library libraries/plupload $WWW_CODE
-  # Feature modules
-  deploy_d7_theme themes/wwwevergreen $WWW_CODE
-  deploy_d7_custom_module modules/custom/evergreen_content $WWW_CODE
-  deploy_d7_custom_module modules/custom/campus_calendar $WWW_CODE
-  echo "www site updates"
-  run_site_updates $WWW_CODE site_updates_www
-fi
 
--- bin/ci_deploy_www.sh
+++ bin/ci_deploy_www.sh
@@ -7,14 +7,12 @@ export WWW_CODE='/var/www/html/www'
 # Deploy Themes
 echo "Beginning WWW Deployment"
 rsync -rtp --delete etc/ $HOME/etc
-if [ -f $WWW_CODE/index.php ]; then
-  # Required Libraries
-  deploy_d7_library libraries/iCalcreator $WWW_CODE
-  deploy_d7_library libraries/plupload $WWW_CODE
-  # Feature modules
-  deploy_d7_theme themes/wwwevergreen $WWW_CODE
-  deploy_d7_custom_module modules/custom/evergreen_content $WWW_CODE
-  deploy_d7_custom_module modules/custom/campus_calendar $WWW_CODE
-  echo "www site updates"
-  run_site_updates $WWW_CODE site_updates_www
-fi
+#
+# Required Libraries
+deploy_d7_library libraries/iCalcreator $WWW_CODE
+deploy_d7_library libraries/plupload $WWW_CODE
+# Feature modules
+deploy_d7_theme themes/wwwevergreen $WWW_CODE
+deploy_d7_custom_module modules/custom/evergreen_content $WWW_CODE
+echo "www site updates"
+run_site_updates $WWW_CODE site_updates_www
