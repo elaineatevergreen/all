f14d400a5ae8afa678366ab609f753a155d48ceb
David Metzler <metzlerd@evergreen.edu>
2016-10-07 11:58:20 -0700

    Correct staging environment detection.

--- site_updates_collab/rev.2016-10-002.feature-revert.sh
+++ site_updates_collab/rev.2016-10-002.feature-revert.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
+set -e
 # Should fix issue with Evergreen CAS settings Feature
 drush --yes dl admin_views
 # commenting this out because it would only apply to the main site.
 # drush fr evergreen_cas --yes
 # drush cc all
-drush --yes collab${SITE_PREFIX}.evergreen.edu.bioregion fr evergreen_cas
-drush --yes collab${SITE_PREFIX}.evergreen.edu.bioregion cc all
+drush --yes collab${SITE_SUFFIX}.evergreen.edu.bioregion fr evergreen_cas
+drush --yes collab${SITE_SUFFIX}.evergreen.edu.bioregion cc all
