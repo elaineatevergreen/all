d0a078805f133aa27d2646c00fdbf4900eb7f67e
David Metzler <metzlerd@evergreen.edu>
2016-10-07 08:38:14 -0700

    Altering build environment for blackmesh boxes.

--- bin/ci_deploy_www.sh
+++ bin/ci_deploy_www.sh
@@ -2,17 +2,9 @@
 set -e
 . bin/www_build_environment.sh
 export WWW_CODE='/var/www/html/www'
-# Source adminweb environment.
-. /opt/evergreen/ods/bin/collab_build_environment.sh
-. drupal_deploy_functions.sh
-. d7_migrations.sh
+. bin/drupal_deploy_functions.sh
+. bin/d7_migrations.sh
 # Deploy Themes
-deploy_d7_theme themes/wwwevergreen $COLLAB_CODE/drupal7
-deploy_d7_module modules/custom/learning_community_directory $COLLAB_CODE/drupal7
-deploy_d7_module modules/custom/native_cases $COLLAB_CODE/drupal7
-echo "Collab Site updates"
-run_site_updates $COLLAB_CODE/drupal7 site_updates_collab
-# www deployment.
 echo "Beginning WWW Deployment"
 if [ -f $WWW_CODE/index.php ]; then
   # Required Libraries
--- bin/www_build_environment.sh
+++ bin/www_build_environment.sh
@@ -1,2 +1,19 @@
 #!/usr/bin/env bash
-
+export PATH=$HOME/bin:$HOME/.composer/vendor/bin:$PATH
+export WWW_CODE='/var/www/wwwdev.evergreen.edu'
+case "$CI_BUILD_REF_NAME" in
+  *test*)
+    STAGE="test"
+    export WWW_CODE='/var/www/wwwtest.evergreen.edu'
+    export SITE_SUFFIX="test";
+    ;;
+  *master*)
+    STAGE="prod"
+    export WWW_CODE='/var/www/www.evergreen.edu'
+    export SITE_SUFFIX="";
+    ;;
+  *)
+    STAGE="dev"
+    export SITE_SUFFIX="dev";
+    ;;
+esac
