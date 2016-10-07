b01f99d138985fa20085484de26ded91d321b054
David Metzler <metzlerd@evergreen.edu>
2016-10-07 11:44:19 -0700

    Comment out cause it woudl be the wrong site.

--- site_updates_collab/rev.2016-10-001.feature-revert.sh
+++ site_updates_collab/rev.2016-10-001.feature-revert.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # Should fix issue with Evergreen CAS settings Feature
 drush --yes dl admin_views
-drush fr evergreen_cas --yes
-drush cc all
+# commenting this out because it would only apply to the main site.
+# drush fr evergreen_cas --yes
+# drush cc all
