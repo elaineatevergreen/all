fac8e658ac5dec07ae37f6abf532697beda337dd
Justin McDowell <ragdoll@revoltpuppy.com>
2016-10-13 16:03:58 -0700

    Ignore superfluous jquery.css file.

--- themes/wwwevergreen/Gruntfile.js
+++ themes/wwwevergreen/Gruntfile.js
@@ -295,10 +295,10 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-imagemin');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-newer');
-	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-postcss');
 	grunt.loadNpmTasks('grunt-perfbudget');
 	grunt.loadNpmTasks('grunt-sass-lint');
