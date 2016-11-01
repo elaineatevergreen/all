300cc7c81ad1e53bd60003dc1285835ff51800c0
Elaine Nelson <me@elainenelson.org>
2016-11-01 11:52:20 -0700

    Oh and actually disable/remove the module
    I just committed the removal from evergreen_content’s .info file.

--- site_updates_www/rev.2016-10-027.remove-module.sh
+++ site_updates_www/rev.2016-10-027.remove-module.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -e
+drush pm-disable custom_search_blocks --yes
+drush pm-uninstall custom_search_blocks --yes
 drush pm-disable custom_search --yes
 drush pm-uninstall custom_search --yes
 drush --yes cc all
