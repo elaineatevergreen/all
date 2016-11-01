8108bb9fb365c437ec68b606ae03d0fb12f7f7c2
Elaine Nelson <me@elainenelson.org>
2016-11-01 16:15:02 -0700

    Update to file migration in preparation to merge to test

--- site_updates_www/rev.2016-10-025.migrate-files-rollback.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-drush mr SiteFiles --yes
-drush --yes cc all
-
--- site_updates_www/rev.2016-10-028.migrate-files.sh
+++ site_updates_www/rev.2016-10-028.migrate-files.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
+#!/usr/bin/env bash
+set -e
 drush mi SiteFiles --yes
 drush --yes cc all
-
