6603411e4c6416501be2f05d335019e57cde6197
David Metzler <metzlerd@evergreen.edu>
2016-11-21 15:58:20 -0800

    Fix to deploy themes.

--- bin/execute_prod_updates.sh
+++ bin/execute_prod_updates.sh
@@ -7,6 +7,7 @@ export WWW_CODE=/var/www/evergreen.edu/htdocs
 export SITE_SUFFIX=""
 # push the site files to production
 rsync -rtp --delete $WWW_CODE/sites/all/modules/custom/ 860elwb01:$WWW_CODE/sites/all/modules/custom
+rsync -rtp --delete $WWW_CODE/sites/all/themes/ 860elwb01:$WWW_CODE/sites/all/themes
 ssh 860elwb01 "bin/prod_site_updates.sh"
 rsync -rtp --delete 860elwb01:$WWW_CODE/sites/all/ $WWW_CODE/sites/all
 rsync -rtp --delete $WWW_CODE/sites/all/ 860elwb01:$WWW_CODE/sites/all
