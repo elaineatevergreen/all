8ea44f5cdbf24a3b45dad99567ea1fddf3b7dac4
Dave Metzler <metzlerd@evergreen.edu>
2016-11-21 15:35:49 -0800

    Update rev.2016-10-002.feature-revert.sh

--- site_updates_collab/rev.2016-10-002.feature-revert.sh
+++ site_updates_collab/rev.2016-10-002.feature-revert.sh
@@ -5,7 +5,7 @@ drush --yes dl admin_views
 # commenting this out because it would only apply to the main site.
 # drush fr evergreen_cas --yes
 # drush cc all
-cd sites/collab${SITE_SUFFIX}.evergreen.edu.bioregion
-drush --yes en evergreen_cas
-drush --yes fr evergreen_cas
-drush --yes cc all
+#cd sites/collab${SITE_SUFFIX}.evergreen.edu.bioregion
+#drush --yes en evergreen_cas
+#drush --yes fr evergreen_cas
+#drush --yes cc all
