0afd6ff1e028fcd3f29169ec90f47804c67b1c6a
David Metzler <metzlerd@evergreen.edu>
2016-11-21 09:41:46 -0800

    Final production config.

--- bin/execute_prod_updates.sh
+++ bin/execute_prod_updates.sh
@@ -7,6 +7,6 @@ export WWW_CODE=/var/www/evergreen.edu/htdocs
 export SITE_SUFFIX=""
 # push the site files to production
 rsync -rtp --delete $WWW_CODE/sites/all/modules/custom/ 860elwb01:$WWW_CODE/sites/all/modules/custom
-ssh 860elwb01 "prod_site_updates.sh"
+ssh 860elwb01 "bin/prod_site_updates.sh"
 rsync -rtp --delete 860elwb01:$WWW_CODE/sites/all/ $WWW_CODE/sites/all
 rsync -rtp --delete $WWW_CODE/sites/all/ 860elwb01:$WWW_CODE/sites/all
