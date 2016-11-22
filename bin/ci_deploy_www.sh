023d6f0efa398c0b3da2a10d29245f9f1e0ef650
David Metzler <metzlerd@evergreen.edu>
2016-11-21 16:08:37 -0800

    Run updates remotely.

--- bin/ci_deploy_www.sh
+++ bin/ci_deploy_www.sh
@@ -22,10 +22,11 @@ deploy_d7_custom_module modules/custom/evergreen_cas $WWW_CODE
 
 # Perform Production updates.
 if [[ "$STAGE" = "prod" ]] ; then
+
   rsync -rtp --delete $HOME/bin/ www_deploy@860elwb01:./bin
   rsync -rtp --delete $HOME/bin/ www_deploy@860elwb02:./bin
   rsync -rtp --delete $HOME/etc/ www_deploy@860elwb01:./etc
   rsync -rtp --delete $HOME/etc/ www_deploy@860elwb02:./etc
   rsync -rtp --delete site_updates_www/ www_deploy@860elwb01:./site_updates_www
-  run_site_updates $WWW_CODE site_updates_www
+  execute_prod_updates.sh
 fi
