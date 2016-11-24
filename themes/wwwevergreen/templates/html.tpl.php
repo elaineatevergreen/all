0c0b57e26bebf1d9bb6fe5d5c24620eb051643a7
Justin McDowell <ragdoll@revoltpuppy.com>
2016-11-23 17:03:08 -0800

    Activates Typekit.

--- themes/wwwevergreen/templates/html.tpl.php
+++ themes/wwwevergreen/templates/html.tpl.php
@@ -90,16 +90,8 @@ Right now I think the only difference from the default html.tpl.php is the inclu
 	</script>
 	
 	<!-- Typekit -->
-	<script>/*
-	  (function(d) {
-	    var config = {
-	      kitId: 'rtm8ksn',
-	      scriptTimeout: 3000
-	    },
-	    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='//use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-	  })(document);
-	</script>
-	<script>try{Typekit.load();}catch(e){}*/</script>
+	<script src="https://use.typekit.net/rtm8ksn.js"></script>
+	<script>try{Typekit.load({ async: true });}catch(e){}</script>
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
   <div id="skip-link">
