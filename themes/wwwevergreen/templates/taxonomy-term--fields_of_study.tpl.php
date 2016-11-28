8250dde4409507befb2151ea37a04c6e78a138a8
Elaine Nelson <me@elainenelson.org>
2016-11-28 09:07:31 -0800

    Updates fields of study page template

--- themes/wwwevergreen/templates/taxonomy-term--fields_of_study.tpl.php
+++ themes/wwwevergreen/templates/taxonomy-term--fields_of_study.tpl.php
@@ -11,14 +11,21 @@ Also, the secondary content is all set via Views+Context.
 
 <?php print render($content['description']); ?>
 
+<?php if(isset($content['field_sample_program_description'])) { ?>
 <h2>Sample Program</h2>
 <?php print render($content['field_sample_program_description']); ?>
+<?php }; ?>
 
-<h2>Offerings</h2>
-<p><em>This will be something else. Soon. I swear.</em></p>
 
+<!--<h2>Offerings</h2>
+<p><em>This will be something else. Soon. I swear.</em></p>-->
+
+<?php if(isset($content['field_after_graduation'])) { ?>
 <h2>After Graduation</h2>
 <?php print render($content['field_after_graduation']); ?>
+<?php }; ?>
 
+<?php if(isset($content['field_facilities_resources'])) { ?>
 <h2>Facilities and Resources</h2>
 <?php print render($content['field_facilities_resources']); ?>
+<?php }; ?>
