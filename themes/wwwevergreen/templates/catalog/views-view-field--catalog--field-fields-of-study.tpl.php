863893d69e56e4c997c2ef1e9450e395f6003f26
Elaine Nelson <me@elainenelson.org>
2016-12-02 11:02:17 -0800

    Syntax fix.

--- themes/wwwevergreen/templates/catalog/views-view-field--catalog--field-fields-of-study.tpl.php
+++ themes/wwwevergreen/templates/catalog/views-view-field--catalog--field-fields-of-study.tpl.php
@@ -8,13 +8,14 @@ Formatting for the fields of study in the catalog index.
 	<ul class="element-list">
 <?php 
 	//print $output;
-	if($output) {
+if($output) {
     //dpm($row->field_field_fields_of_study);
     foreach($row->field_field_fields_of_study as $fos) {
 		?>
 		<li><a href="<?php print $fos['rendered']['#href'] ?>"><?php print $fos['rendered']['#title'] ?></a></li>	 
 		<?php   
     };
+};    
 ?>
 	</ul>
 </div>
