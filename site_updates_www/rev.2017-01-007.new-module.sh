565e658f49b94c558914d7df4c801b687f64904a
Elaine Nelson <me@elainenelson.org>
2017-02-14 11:24:40 -0800

    Removes script files that added modules
    Going to use Context instead of these code-adding modules.

--- site_updates_www/rev.2017-01-007.new-module.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-drush en --yes cpn
-drush --yes cc all
-
--- site_updates_www/rev.2017-01-008.new-module.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-drush en --yes css_injector
-drush en --yes js_injector
-drush --yes cc all
-
