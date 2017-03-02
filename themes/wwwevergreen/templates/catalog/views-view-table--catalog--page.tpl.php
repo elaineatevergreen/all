778534803ed803aa77093c95be2ae6dcfe0503f6
Elaine Nelson <me@elainenelson.org>
2017-03-02 11:22:50 -0800

    Updates to catalog display
    Includes template file, content type definition, feed importer, and view.
    
    # Conflicts:
    #	themes/wwwevergreen/templates/catalog/views-view-table--catalog--page.tpl.php

--- themes/wwwevergreen/templates/catalog/views-view-table--catalog--page.tpl.php
+++ themes/wwwevergreen/templates/catalog/views-view-table--catalog--page.tpl.php
@@ -21,7 +21,7 @@
  * @ingroup views_templates
  */
  
-$hiddenfields = array('field_academic_year','field_quarters_open','field_quarters_signature','field_quarters_closed','field_summer_session');
+$hiddenfields = array('field_academic_year','field_summer_session');
  
 ?>
 <table <?php if ($classes) { print 'class="'. $classes . '" '; } ?><?php print $attributes; ?>>
@@ -56,111 +56,62 @@ $hiddenfields = array('field_academic_year','field_quarters_open','field_quarter
 	        /* this is maybe not ideal, but it works to build the complex markup that we want to display on the catalog */
 	        
 			//ACADEMIC YEAR: math it up!
-			//$academicyear = ($row['field_academic_year']-1) . '–' . substr($row['field_academic_year'], 2,2);
-			//$threequarters = substr($row['field_academic_year'],0,-2);
 			//hilariously, because I did some formatting in another template file, I now have to do slightly different munging than before.
 			//data comes over in format 2016–17 vs raw value 2017. also, needs trimming!
 			$fall = substr(trim($row['field_academic_year']),0,4);
 			$threequarters = $fall+1;
 			
-			/*
-				for each quarter offered...
-				add on S if signature required
-				add on C if closed entirely
-				add on the calendar year	
-			*/
+
+			//dpm($row['field_quarters']);
+			//now format all the quarters!
+			$quarters = explode(',', $row['field_quarters']);
 			
-			$quartersoffered = explode(',', $row['field_quarters_offered']);
-			$quarterssig = explode(',', $row['field_quarters_signature']);
-			$quartersopen = explode(',', $row['field_quarters_open']);
-			$quartersclosed = explode(',', $row['field_quarters_closed']);
-			
-			/* original working code 
-			
-			foreach($quartersoffered as $key => $value) {
-				if(in_array($value, $quarterssig)) { 
-					$quartersoffered[$key] = '<abbr title="signature required">' . $value . '&nbsp;(S)</abbr>';
-				};
-				if(in_array($value, $quartersclosed)) { 
-					$quartersoffered[$key] = '<abbr title="enrollment closed">' . $value . '&nbsp;(C)</abbr>'; 
-				};
-			};
-			
-			$printquarters = '<ul>';
-			foreach($quartersoffered as $q) {
-				$printquarters .= "<li>" . trim($q);
-				//if this is summer, add the session info.
-				if($row['field_summer_session'] != '') {
-					$printquarters .= " (" . trim($row['field_summer_session']) . "&nbsp;Session)";
-				}
-				$printquarters .= "</li>";
-			};
-			$printquarters .= '</ul>';
-			*/
-			
-			
-			
-			
-			/* experimental code */
-			
-			$quarters = array('Fall','Winter','Spring');
 			$printquarters = '<div class="quarter-indicator-group">';
 			foreach($quarters as $q) {
-				$classname = strtolower($q);
+				$classes = explode(' ', $q);
+				$quarterclass = strtolower($classes[0]);
+				$statusclass = strtolower($classes[1]);
 				
-				//open
-				if(in_array($q,$quartersopen)) { 
-					$printquarters .= "<div class='qi qi-$classname'><abbr title='$q quarter'>$q quarter</abbr></div>"; 
-				}
-				//signature
-				elseif(in_array($q, $quarterssig)) { 
-					$printquarters .= "<div class='qi qi-signature qi-$classname'><abbr title='$q quarter, signature required'>$q quarter, signature required</abbr></div>";
-				}
-				//closed
-				elseif(in_array($q, $quartersclosed)) { 
-					$printquarters .= "<div class='qi qi-closed qi-$classname'><abbr title='$q quarter, enrollment closed'>$value quarter, enrollment closed</abbr></div>";
-				}
-				else {
-					$printquarters .= "<div class='qi qi-signature qi-$classname'></div>";  
-				};
-				
-				
-			};
-			
-			
-			/* summer is special! */
-			
-			//still needs signature info :(
-			
-			$printquarters .= '<div class="summer-indicator-group">';
-			if(trim($row['field_summer_session']) == 'Full') {
-					$printquarters .= '<div class="qi qi-summer-session1'; 
-					if(in_array('Summer',$quarterssig)) { $printquarters .= ' qi-signature '; };
-					$printquarters .= '"><abbr title="Summer Session 1">Summer Session 1</abbr></div><div class="qi qi-summer-session2';
-					if(in_array('Summer',$quarterssig)) { $printquarters .= ' qi-signature '; };
-					$printquarters .= '"><abbr title="">Summer Session 2 (continued)</abbr></div>';
-			}
-			elseif(trim($row['field_summer_session']) == 'First') {
-					$printquarters .= '<div class="qi qi-summer-session1"><abbr title="Summer Session 1">Summer Session 1</abbr></div><div class="qi qi-summer-session2 qi-na"></div>';
-			}
-			elseif(trim($row['field_summer_session']) == 'Second') {
-					$printquarters .= '<div class="qi qi-summer-session1 qi-na"></div><div class="qi qi-summer-session2"><abbr title="Summer Session 2">Summer Session 2</abbr></div>';
-			} else {
-				//blank entry for non-Summer offerings
-				$printquarters .= '<div class="qi qi-summer-session1 qi-na"></div><div class="qi qi-summer-session2 qi-na"></div>';
-			};
+				//if this is summer, show the session info
+				if($row['field_summer_session'] != '') {
+					//do something here.
+					//$printquarters .= " (" . trim($row['field_summer_session']) . "&nbsp;Session)";
+					
+					$printquarters .= '<div class="summer-indicator-group">';
+					$summer = trim($row['field_summer_session']);
+					if($summer == 'Full' or $summer == 'First') {
+						$printquarters .= "<div class='qi qi-summer-session1 qi-$statusclass'>Summer Session 1</div>";
+					} else {
+						$printquarters .= "<div class='qi qi-summer-session1 qi-na'></div>";
+					};
+					if($summer == 'Full' or $summer == 'Second') {
+						$printquarters .= "<div class='qi qi-summer-session2 qi-$statusclass'>Summer Session 2</div>";
+					} else {
+						$printquarters .= '<div class="qi qi-summer-session2 qi-na"></div>';
+					};
+					$printquarters .= '</div>'; //end of group
+					
+					
+				} else {
+					
+					$printquarters .= "<div class='qi qi-$quarterclass qi-$statusclass'><abbr title='$q'>$q</abbr></div>";
+				}; //end check for summer
+			}; //end foreach
 			$printquarters .= '</div>';
-
 			
-			//end the whole goddamn div
-			$printquarters .= "</div>";
-		
-			//add in the calendar year associated with this quarter
+			
+			//add year to each quarter
 			$printquarters = str_replace('Fall', 'Fall&nbsp;' . $fall, $printquarters);
-			
 			$printquarters = str_replace('Winter', 'Winter&nbsp;' . $threequarters, $printquarters);
 			$printquarters = str_replace('Spring', 'Spring&nbsp;' . $threequarters, $printquarters);
 			$printquarters = str_replace('Summer', 'Summer&nbsp;' . $threequarters, $printquarters);
+			
+			//format status text
+			$printquarters = str_replace('Open', '', $printquarters);
+			$printquarters = str_replace('Conditional', '', $printquarters);
+			$printquarters = str_replace('Signature', ' signature required', $printquarters);
+			$printquarters = str_replace('Closed', ' enrollment closed', $printquarters);
+			
 	        
 	        
 	        foreach ($row as $field => $content):
@@ -170,7 +121,7 @@ $hiddenfields = array('field_academic_year','field_quarters_open','field_quarter
 	         ?>
           <td <?php if ($field_classes[$field][$row_count]) { print 'class="'. $field_classes[$field][$row_count] . '" '; } ?><?php print drupal_attributes($field_attributes[$field][$row_count]); ?>>
             <?php 
-	            if ($field == 'field_quarters_offered') {
+	            if ($field == 'field_quarters') {
 		            print($printquarters);
 		        
 		        } else {
