4a00fd4aa2c3d34f6e323694628fbb309dff2e27
Elaine Nelson <me@elainenelson.org>
2017-03-21 11:04:35 -0700

    Updates template for faculty photo on catalog

--- themes/wwwevergreen/templates/directory/node--directory_individual--faculty_blurb.tpl.php
+++ themes/wwwevergreen/templates/directory/node--directory_individual--faculty_blurb.tpl.php
@@ -10,9 +10,23 @@ In this entity view mode, we get a name, a tiny headshot, and their expertise. f
 <div class="unit-1-2">
 <div class="h-card media">
 	<div class="media-img">
-<?php if(isset($content['field_headshot'])) {
+<?php 
+	/* this goes thru all the options for photos... */
+	
+//square portrait is the best option
+if(isset($content['field_square_portrait'])) {
+	print render($content['field_square_portrait']);
+} 
+//second best: the rectangular headshot, which gets CSS-shrunk
+elseif(isset($content['field_headshot'])) {
 	print render($content['field_headshot']);
-} ?>
+} 
+//finally, if no photo use the small square "no photo" image
+else { ?>
+<img src="<?php print base_path() . path_to_theme() ?>/images/directory/_blank-square.png" alt=""/>
+<?php	
+};
+?>
 	</div>
 	
 	<div class="media-body">
