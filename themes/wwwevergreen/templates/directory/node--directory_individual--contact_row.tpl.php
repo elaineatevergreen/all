bb718849da810f24eef5f511b4a6808d2d2f13dd
Elaine Nelson <me@elainenelson.org>
2017-06-05 11:23:35 -0700

    Updates templates for directory individuals
    Hides contact info from anonymous users.

--- themes/wwwevergreen/templates/directory/node--directory_individual--contact_row.tpl.php
+++ themes/wwwevergreen/templates/directory/node--directory_individual--contact_row.tpl.php
@@ -8,7 +8,11 @@ themes each person as a row to go in a table.
 
     <tr>
         <td><strong><?php print render($content['field_first_name']) . ' ' . render($content['field_last_name']) ?></strong>, <?php print render($content['field_job_title']) ?></td>
-        <td><?php print render($content['field_email']) ?></td>
+<?php 
+	//for right now, hide contact information from off-campus users
+	if(user_is_logged_in()) {
+?>
+		<td><?php print render($content['field_email']) ?></td>
         <td><?php print render($content['field_building_alt']) ?> <?php print render($content['field_room']) ?></td>
         <td><?php print render($content['field_mailstop']) ?></td>
         <td>
@@ -22,4 +26,9 @@ themes each person as a row to go in a table.
 	        ?><br><i>Alt:</i> <?php print render($content['field_alternate_phone']) ?><?php
         }; ?>
         </td>
+<?php } else { ?>
+		<td colspan="4">Currently contact information is only available to logged in members of the campus community.</td>
+	
+<?php }; ?>
+         
     </tr>
