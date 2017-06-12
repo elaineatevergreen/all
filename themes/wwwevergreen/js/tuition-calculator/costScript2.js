5739dd2bc0eebcebe9ccf11a1e12662908e243a0
Elaine Nelson <me@elainenelson.org>
2017-06-12 15:42:44 -0700

    Adds path to csv location

--- themes/wwwevergreen/js/tuition-calculator/costScript2.js
+++ themes/wwwevergreen/js/tuition-calculator/costScript2.js
@@ -126,6 +126,7 @@ function clickFunction() {
     if(CSV != document.getElementById('selCSV').options[document.getElementById('selCSV').selectedIndex].value ){
         CSV = document.getElementById('selCSV');
         CSV = CSV.options[CSV.selectedIndex].value;
+        CSV = "/sites/all/modules/wwwevergreen/js/tuition-calculator" + CSV;
         console.log(CSV);
         //gets file name from selected csv 
         $.ajax({
