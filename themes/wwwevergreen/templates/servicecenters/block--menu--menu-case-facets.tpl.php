e60aa5828a91f1e9966967f413b9e196241f6f90
Elaine Nelson <me@elainenelson.org>
2017-06-16 13:45:50 -0700

    Add the subheader back in

--- themes/wwwevergreen/templates/servicecenters/block--menu--menu-case-facets.tpl.php
+++ themes/wwwevergreen/templates/servicecenters/block--menu--menu-case-facets.tpl.php
@@ -7,6 +7,9 @@
  */
 ?>
 <nav id="tertiary-nav" class="box">
+	<?php if ($block->subject): ?>
+  <h4<?php print $title_attributes; ?>><?php print $block->subject ?></h4>
+<?php endif;?>
 	<?php print $content ?>
 </nav>
 
