716f825986524d4837950242b81aacf7c917270b
Justin McDowell <mcdowelj@evergreen.edu>
2017-07-06 14:19:45 -0700

    Debugs panels page theme.

--- themes/wwwevergreen/templates/page--panels--nativecases.tpl.php
+++ themes/wwwevergreen/templates/page--panels--nativecases.tpl.php
@@ -95,52 +95,59 @@ $evergreen_blocks = theme_get_setting('evergreen_blocks');
 			</header>
 		<?php }; ?>
 	
-		<?php 
-			/* for Panels pages, there are no wrapping grid divs. */ 
-			/* should the tertiary-nav-wrapper also go away? */
-		?>
-	
-		<div class="tertiary-nav-wrapper unit-1-7">
-			<?php 
-				/* render anything that's in the section navigation. see region--section_nav.tpl.php for that markup. */
-				print render($page['section_nav']); 
-				
-				/* then any 1st column content outside of the nav. usually search filters? */
-				print render($page['filters']); 
-			?>
-		</div>
-		<?php /* for Panels pages, there are no wrapping grid divs. */ ?>
-		<?php print $messages; ?>
-		<?php if ($page['highlighted']): ?>
-			<div id="highlighted">
-				<?php print render($page['highlighted']); ?>
+		<div class="grid">
+			<div class="unit-1-1">
+				<!-- "Logged in via CAS." -->
+				<?php print $messages; ?>
 			</div>
-		<?php endif; ?>
-		<div id="main-content"></div>
-		<?php print render($title_prefix); ?>
-		<?php if ($title and $title!='Home'): ?>
-			<h1 class="title" id="page-title">
-				<?php print $title; ?>
-			</h1>
-		<?php endif; ?>
-		<?php print render($title_suffix); ?>
-		<!-- No tabs on panels pages.
-		<?php if ($tabs): ?>
-			<div class="tabs">
-				<?php print render($tabs); ?>
+			<div class="tertiary-nav-wrapper unit-1-7">
+				<?php 
+					/* render anything that's in the section navigation. see region--section_nav.tpl.php for that markup. */
+					print render($page['section_nav']); 
+					
+					/* then any 1st column content outside of the nav. usually search filters? */
+					print render($page['filters']); 
+				?>
 			</div>
-		<?php endif; ?>-->
-		
-		<?php print render($page['help']); ?>
-		<?php if ($action_links): ?>
-			<ul class="action-links">
-				<?php print render($action_links); ?>
-			</ul>
-		<?php endif; ?>
-		<!-- Begin render page content -->
-		<?php print render($page['content']); ?>
-		<!-- End render page content -->
-		<?php print $feed_icons; ?>
+			<?php /* for Panels pages, there are no wrapping grid divs. */ ?>
+			<!-- What is highlighted? -jkm -->
+			<?php if ($page['highlighted']): ?>
+				<div id="highlighted">
+					<?php print render($page['highlighted']); ?>
+				</div>
+			<?php endif; ?>
+			<div id="main-content"></div>
+			<?php print render($title_prefix); ?>
+			<?php if ($title and $title!='Home'): ?>
+				<h1 class="title" id="page-title">
+					<?php print $title; ?>
+				</h1>
+			<?php endif; ?>
+			<?php print render($title_suffix); ?>
+			<!-- No tabs on panels pages.
+			<?php if ($tabs): ?>
+				<div class="tabs">
+					<?php print render($tabs); ?>
+				</div>
+			<?php endif; ?>-->
+			
+			<!-- What is help? —jkm -->
+			<?php print render($page['help']); ?>
+			
+			<!-- What are action-links? —jkm -->
+			<?php if ($action_links): ?>
+				<ul class="action-links">
+					<?php print render($action_links); ?>
+				</ul>
+			<?php endif; ?>
+			
+			<!-- Begin render page content -->
+			<?php print render($page['content']); ?>
+			<!-- End render page content -->
+			
+			<!-- What feed icons? —jkm -->
+			<?php print $feed_icons; ?>
+		</div><!-- /.grid -->
 	</main>
 </section>
 
