71d7fffcd59ec960ad992aeabbf5f8026e8e328e
Elaine Nelson <nelsone@evergreen.edu>
2017-10-16 09:16:06 -0700

    Adds evergreen policies feature to deploy script

--- bin/ci_deploy_www.sh
+++ bin/ci_deploy_www.sh
@@ -28,6 +28,7 @@ deploy_d7_custom_module modules/custom/learning_community_directory $WWW_CODE
 deploy_d7_custom_module modules/custom/native_cases $WWW_CODE
 deploy_d7_custom_module modules/custom/simple_events $WWW_CODE
 deploy_d7_custom_module modules/custom/search_api_solr_booster $WWW_CODE
+deploy_d7_custom_module modules/custom/evergreen_policies $WWW_CODE
 rsync .htaccess $WWW_CODE/
 
 # Perform Production updates.
