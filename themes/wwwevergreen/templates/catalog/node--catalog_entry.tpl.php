e832d63f61c9047442c2aac25cd49b7a9cafd97a
Justin McDowell <mcdowelj@evergreen.edu>
2017-11-07 11:33:10 -0800

    Updates structure of catalog entry credits.

--- themes/wwwevergreen/templates/catalog/node--catalog_entry.tpl.php
+++ themes/wwwevergreen/templates/catalog/node--catalog_entry.tpl.php
@@ -174,145 +174,147 @@
 
   <div class="content"<?php print $content_attributes; ?>>
     <?php
-      // We hide the comments and links now so that we can render them later.
-		hide($content['comments']);
-		hide($content['links']);
-		hide($content['field_academic_year']);
-		hide($content['field_quarters_offered']);
-		hide($content['field_faculty']);
-		hide($content['field_revisions']);
-		hide($content['group_details']['group_location_schedule']);
-		hide($content['group_details']['group_more']);
-
-
-	$updatestatus = '';
-	if(strstr(render($content['field_offering_status']),'Cancelled')) {
-		$updatestatus = 'CANCELLED';
-	} elseif((time()-(60*60*24*30)) < strtotime($content['field_status_date']['#items'][0]['value'])) {
-		$updatestatus = 'NEW';
-	} elseif((time()-(60*60*24*30)) < strtotime($content['field_revision_date']['#items'][0]['value'])) {
-		$updatestatus = 'REVISED';
-	};
-
-
-	if($updatestatus != '') {
-	 ?>
-	<div class="box note program-status">
-		<p class="program-status-revised"><?php print $updatestatus ?></p>
-	</div>
-	<?php }; ?>
-	<?php // COMMENTING OUT THE OLD QUARTERS GREEN TITLE ?>
-	 <!-- <p class="intro"><?php //print($quarters_intro); ?></p> -->
-
-	 <div class="program-description">
-
-		 <p>Taught by</p>
-		 <div class="grid">
-			 <?php print render($content['field_faculty']); ?>
-		 </div>
-
-     <?php // changed $content to now just print the body content without the save link attached
-      print render($content['body'][0]); ?>
-
-     <?php //Starting new FANCY bottom section ?>
-
-
-
-
-
-
-     <!--  //Fields of study standin
-     	// field_fields_of_study (NEED TO TEST WITH ONE THAT ACTUALLY HAS THIS FIELD)-->
-     	<?php if(isset($content['group_details']['field_fields_of_study'][0])) { ?>
-     		  <br>
-     		  <b><?php print ("Fields of Study:")?></b>
-	          <?php print_r(render($content['group_details']['field_fields_of_study'][0])); ?>
-	    <?php }; ?>
-     <!--// Preparatory Fields standin
-        // field_preparatory_for -->
-
-     	<?php if(isset($content['group_details']['field_preparatory_for'][0])) { ?>
-     	      <br>
-	          <b><?php print ("This offering will prepare you for careers in:")?></b>
-	          <?php print_r(render($content['group_details']['field_preparatory_for'][0])); ?>
-	    <?php }; ?>
-	<!--
-     // Maximum enrollment standin
-     	// field_maximum_enrollment -->
-
-     	<?php if(isset($content['field_maximum_enrollment'][0])) { ?>
-     		  <br>
-     	      <br>
-	          <b><?php print ("Maximum enrollment:")?></b>
-	          <?php print_r(render($content['field_maximum_enrollment'][0])); ?>
-	    <?php }; ?>
-    <!--
-     // Online Learning standin
-     	// field_online_learning -->
-
-     	<?php if(isset($content['group_details']['group_more']['field_online_learning'][0])) { ?>
-     	      <br>
-	          <b><?php print ("Online learning:")?></b>
-	          <?php print_r(render($content['group_details']['group_more']['field_online_learning'][0])); ?>
-	    <?php }; ?>
-    <!--
-     // Special expenses
-     	// field_special_expenses -->
-
-     	<?php if(isset($content['group_details']['group_more']['field_special_expenses'][0])) { ?>
-     	      <br>
-	          <b><?php print ("Special expenses:")?></b>
-	          <?php print_r(render($content['group_details']['group_more']['field_special_expenses'][0])); ?>
-	    <?php }; ?>
-    <!--
-     // Fees standin
-     	// field_fees (can be 0?) -->
-
-     	<?php if(isset($content['group_details']['group_more']['field_fees'][0])) { ?>
-     		  <br>
-	          <b><?php print ("Fees:") // have to do a substr to get rid of annoying paragraph tabs below ?></b>
-	          <?php print (substr(render($content['group_details']['group_more']['field_fees'][0]), 3, -4)); ?>
-	    <?php }; ?>
-    <!--
-     // Upper Div Sci credit standin
-     	//field_upper_division (field_upper_division_boolean seems to be 1 on classes without upper credit too?)-->
-
-     	<?php if(isset($content['field_upper_division'][0])) { ?>
-     	      <br>
-	          <b><?php print ("Upper Division Science Credit:") // also getting rid of annoying p tags below?></b>
-	          <?php print (substr(render($content['field_upper_division'][0]), 3, -4)); ?>
-	    <?php }; ?>
-
-    <!--
-     //REGISTER FOR THIS OFFERING H1 -->
-		 <h2>Register for this Offering</h2>
-		 <!--
-	 	 // Variable credit
-	 	//field_variable_credit_options (field_upper_division_boolean seems to be 1 on classes without upper credit too?)-->
-		<?php if(isset($content['field_variable_credit_options'][0])) { ?>
-					<h3>Variable Credit Options</h3>
-					<br>
-					<?php print_r(render($content['field_variable_credit_options'][0])); ?>
+			// We hide the comments and links now so that we can render them later.
+			hide($content['comments']);
+			hide($content['links']);
+			hide($content['field_academic_year']);
+			hide($content['field_quarters_offered']);
+			hide($content['field_faculty']);
+			hide($content['field_revisions']);
+			hide($content['group_details']['group_location_schedule']);
+			hide($content['group_details']['group_more']);
+	
+			$updatestatus = '';
+			if(strstr(render($content['field_offering_status']),'Cancelled')) {
+				$updatestatus = 'CANCELLED';
+			} elseif((time()-(60*60*24*30)) < strtotime($content['field_status_date']['#items'][0]['value'])) {
+				$updatestatus = 'NEW';
+			} elseif((time()-(60*60*24*30)) < strtotime($content['field_revision_date']['#items'][0]['value'])) {
+				$updatestatus = 'REVISED';
+			};
+	
+			if($updatestatus != '') {
+		?>
+		<div class="box note program-status">
+			<p class="program-status-revised"><?php print $updatestatus ?></p>
+		</div>
 		<?php }; ?>
-		 <h3>How to Register</h3>
-		 <br>
-		 1. Copy the course reference number (CRN) for your class standing and desired number of credits.
-		 <br>
-		 <br>
-		 2. Use your CRN at <b>my.evergreen.edu</b> during your registration window. Check the academic calendar for <b>upcoming registration deadlines.</b>
-		 <br>
-		 <br>
-		 Learn more about <b>how to register</b>, including information about registering as a non-admitted (special) student.
-		 <br>
-    <!--
-     // Course Reference Numbers Standin
-     //	sidebar stuff here -->
-     <h3>Course Reference Numbers</h3>
-		 <!-- variable for dropping the H4 signature required to a nice italics version with a colon if needed-->
-		 <?php $sig_required_h4 = "<h4>Signature Required</h4>"?>
-		 <?php $sig_required_h4_italics = "<p><i>Signature Required:</i>"?>
 
-			 <!-- fall REGISTRATION -->
+		<?php // COMMENTING OUT THE OLD QUARTERS GREEN TITLE ?>
+		 <!-- <p class="intro"><?php //print($quarters_intro); ?></p> -->
+
+
+		<?php
+		/**
+		 * Program Description
+		 */ 
+		?>
+		<div class="program-description">
+			<?php
+			/**
+			 * Faculty List
+			 */
+			?>
+			<p>Taught by</p>
+			<div class="grid">
+				<?php print render($content['field_faculty']); ?>
+			</div>
+			
+			<?php
+			/**
+			 * Main Program Description
+			 *
+			 * This section prints the longhand program description as
+			 * provided by faculty.
+			 */
+			?>
+			<?php // changed $content to now just print the body content without the save link attached
+			print render($content['body'][0]); ?>
+			
+			<?php
+			/**
+			 * Program Details
+			 */
+			?>
+			<?php // Fields of study standin
+			// field_fields_of_study (NEED TO TEST WITH ONE THAT ACTUALLY HAS THIS FIELD) ?>
+     	<?php if(isset($content['group_details']['field_fields_of_study'][0])) { ?>
+				<p><b><?php print ("Fields of Study:")?></b> <?php print_r(render($content['group_details']['field_fields_of_study'][0])); ?></p>
+	    <?php }; ?>
+	    
+	    <?php // Preparatory Fields standin
+			// field_preparatory_for ?>
+			<?php if(isset($content['group_details']['field_preparatory_for'][0])) { ?>
+				<p><b><?php print ("This offering will prepare you for careers and advanced study in:")?></b> <?php print_r(render($content['group_details']['field_preparatory_for'][0])); ?></p>
+			<?php }; ?>
+			
+			<?php // Maximum enrollment standin
+			// field_maximum_enrollment ?>
+			<?php if(isset($content['field_maximum_enrollment'][0])) { ?>
+				<p><b><?php print ("Maximum enrollment:")?></b> <?php print_r(render($content['field_maximum_enrollment'][0])); ?></p>
+	    <?php }; ?>
+	    
+			<?php // Online Learning standin
+			// field_online_learning ?>
+			<?php if(isset($content['group_details']['group_more']['field_online_learning'][0])) { ?>
+				<div><b><?php print ("Online learning:")?></b> <?php print_r(render($content['group_details']['group_more']['field_online_learning'][0])); ?></div>
+	    <?php }; ?>
+	    
+	    <?php // Special expenses
+     	// field_special_expenses ?>
+     	<?php if(isset($content['group_details']['group_more']['field_special_expenses'][0])) { ?>
+				<div><b><?php print ("Special expenses:")?></b> <?php print_r(render($content['group_details']['group_more']['field_special_expenses'][0])); ?></div>
+	    <?php }; ?>
+
+			<?php // Fees standin
+			// field_fees (can be 0?) ?>
+			<?php if(isset($content['group_details']['group_more']['field_fees'][0])) { ?>
+				<div><b><?php print ("Fees:") // have to do a substr to get rid of annoying paragraph tabs below ?></b> <?php print (substr(render($content['group_details']['group_more']['field_fees'][0]), 3, -4)); ?></div>
+	    <?php }; ?>
+
+	    <?php // Upper Div Sci credit standin
+			// field_upper_division (field_upper_division_boolean seems to be 1 on classes without upper credit too?) ?>
+			<?php if(isset($content['field_upper_division'][0])) { ?>
+				<p><b><?php print ("Upper Division Science Credit:") // also getting rid of annoying p tags below?></b> <?php print (substr(render($content['field_upper_division'][0]), 3, -4)); ?></p>
+	    <?php }; ?>
+
+			<?php
+			/**
+			 * Registration
+			 */
+			?>
+			<h2>Register for this Offering</h2>
+			<?php // Variable credit
+			// field_variable_credit_options (field_upper_division_boolean seems to be 1 on classes without upper credit too?)
+			?>
+			<?php if(isset($content['field_variable_credit_options'][0])) { ?>
+				<h3>Variable Credit Options</h3>
+				<?php print_r(render($content['field_variable_credit_options'][0])); ?>
+			<?php }; ?>
+			<h3>How to Register</h3>
+			<ol>
+				<li>
+					<p>Copy the course reference number (CRN) for your class standing and desired number of credits.</p>
+				</li>
+				<li>
+					<p>Use your CRN at <a href="https://my.evergreen.edu">my.evergreen.edu</a> during your registration window. Check the academic calendar for <a href="/calendar/academic">upcoming registration deadlines</a>.</p>
+				</li>
+			</ol>
+			<p>Learn more about <a href="/registration/how-to">how to register</a>, including information about registering as a non-admitted (special) student.</p>
+			
+			<?php
+			/**
+			 * Course Reference Numbers
+			 */
+			?>
+			<?php // Course Reference Numbers Standin
+			// sidebar stuff here ?>
+			<h3>Course Reference Numbers</h3>
+			<!-- variable for dropping the H4 signature required to a nice italics version with a colon if needed-->
+			<?php $sig_required_h4 = "<h4>Signature Required</h4>"?>
+			<?php $sig_required_h4_italics = "<p><i>Signature Required:</i>"?>
+
+			 <!-- fall Registration -->
 			 <?php if(isset($content['field_fall_registration'])) { ?>
 			   <h4>Fall Quarter</h4>
 			   <br>
