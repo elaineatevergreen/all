07634cf6e0092f184d24078ad0297215a62351c8
Steven Meaney <meaneys@evergreen.edu>
2017-11-08 15:28:01 -0800

    Adding the node policy php file to see if we can get it to cause changes

---
+++ themes/wwwevergreen/templates/node--policy.tpl.php
@@ -0,0 +1,5 @@
+<?php
+  // testing to see if this is actually being read on the policies pages
+  hide($content['field_archived']);
+  hide($content['body']);?>
+  <?php print render("TEST TEST TEST TEST TEST TEST TEST TEST TEST"); ?>
