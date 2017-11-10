82ca98a99b64b04a53f638883826a2a4d526acf5
Steven Meaney <meaneys@evergreen.edu>
2017-11-09 16:05:19 -0800

    testing

--- themes/wwwevergreen/templates/node--policy.tpl.php
+++ themes/wwwevergreen/templates/node--policy.tpl.php
@@ -1,2 +1,26 @@
-<?php print render($content); ?>
+<?php 
+
+print render($content['field_prefix']);
+print render($content['title']);
+print render($content['field_categories']);
+print render($content['field_effective_date']);
+print render($content['field_archived']);
+print render($content['field_updated_version']);
+print render($content['field_external_policy']);
+print render($content['body']);
+print render($content['group_related']);
+print render($content['field_related_documents']);
+print render($content['field_related_policies']);
+print render($content['field_previous_version']);
+print render($content['field_stub']);
+print render($content['path']);
+print render($content['field_approval_date']);
+print render($content['field_approval_authority']);
+print render($content['field_signature_file']);
+print render($content['field_steward']);
+
+
+
+?>
+
 <?php print render($content); ?>
