a1a9c9db0a42073700439d3202f2ef528cac916c
Steven Meaney <meaneys@evergreen.edu>
2017-11-15 15:13:16 -0800

    Update node--policy.tpl.php

--- themes/wwwevergreen/templates/node--policy.tpl.php
+++ themes/wwwevergreen/templates/node--policy.tpl.php
@@ -1,17 +1,5 @@
-<?php
-print render($content['field_prefix']);
-print render($content['title']);
-print render($content['field_categories']);
-?>
 
-
-<?php if (!empty($content['field_effective_date'])): ?>
-    <div class="content">
-      <div>
-         <?php print render($content['field_effective_date']); ?>
-      </div>
-    </div>
-<?php endif;?>
+<?phpprint render($content['title']);?>
 
 
 <?php if (!empty($content['field_archived'])): ?>
@@ -22,16 +10,23 @@ print render($content['field_categories']);
     </div>
 <?php endif;?>
 
-<?php
-print render($content['field_updated_version']);
-print render($content['field_external_policy']);
-print render($content['body']);
-print render($content['group_related']);
-print render($content['field_related_documents']);
-print render($content['field_related_policies']);
-print render($content['field_previous_version']);
-print render($content['field_stub']);
-print render($content['path']); ?>
+
+<?phpprint render($content['field_prefix']);?>
+
+
+
+
+
+
+<?php print render($content['field_updated_version']); ?>
+<?php print render($content['field_external_policy']); ?>
+
+<?php print render($content['group_related']); ?>
+<?php print render($content['field_related_documents']); ?>
+<?php print render($content['field_related_policies']); ?>
+<?php print render($content['field_previous_version']); ?>
+<?php print render($content['field_stub']); ?>
+<?php print render($content['path']); ?>
 
 <?php if (!empty($content['field_approval_date'])): ?>
     <div class="content">
@@ -49,9 +44,10 @@ print render($content['path']); ?>
     </div>
 <?php endif;?>
 
-<?php
-print render($content['field_signature_file']);
-print render($content['field_steward']);
-?>
+<?php print render($content['field_signature_file']); ?>
+<?php print render($content['field_steward']); ?>
+
+<?php print render($content['field_categories']); ?>
+<?php print render($content['body']); ?>
 
 
