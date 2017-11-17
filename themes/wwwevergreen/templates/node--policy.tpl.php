da21371accf202788b1642de1406576d8405dab8
Steven Meaney <meaneys@evergreen.edu>
2017-11-17 09:49:19 -0800

    Update node--policy.tpl.php

--- themes/wwwevergreen/templates/node--policy.tpl.php
+++ themes/wwwevergreen/templates/node--policy.tpl.php
@@ -78,8 +78,7 @@
 
 <?php print render($content['body']); ?>
 
-<?php print render($content['group_related']); ?>
-<?php print render($content['group_related']['field_related_policies']); ?>
-<?php print render($content['group_related']['field_related_documents']); ?>
+<?php print render($content['field_related_policies']); ?>
+<?php print render($content['field_related_documents']); ?>
 
 
