0b538b9a5470e1d7997b342dcc618ccb66bfd5a8
MeaneyS <meaneys@evergreen.edu>
2018-02-15 15:35:00 -0800

    testing non-loop version of varcredit

--- themes/wwwevergreen/templates/catalog/node--catalog_entry.tpl.php
+++ themes/wwwevergreen/templates/catalog/node--catalog_entry.tpl.php
@@ -238,10 +238,25 @@ if(count($quarters) == 1) {
 			<img alt="Variable"
 			     src="/sites/all/themes/wwwevergreen/images/icons/catalog/credits-variable.svg"/>
 		<?php }else{ ?>
-			<img alt="<?php print(render($content['group_details']['field_credits'][0]))?>"
-			     src="/sites/all/themes/wwwevergreen/images/icons/catalog/credits-<?php print(render($content['group_details']['field_credits'][0]))?>.svg"/>
-		<?php } ?>
+			<? if(isset($content['group_details']['field_credits'][0])){ ?>
+				<img alt="<?php print(render($content['group_details']['field_credits'][0]))?>"
+						 src="/sites/all/themes/wwwevergreen/images/icons/catalog/credits-<?php print(render($content['group_details']['field_credits'][0]))?>.svg"/>
+			<?php } ?>
+			<? if(isset($content['group_details']['field_credits'][1])){ ?>
+				<img alt="<?php print(render($content['group_details']['field_credits'][1]))?>"
+						 src="/sites/all/themes/wwwevergreen/images/icons/catalog/credits-<?php print(render($content['group_details']['field_credits'][1]))?>.svg"/>
+			<?php } ?>
+			<? if(isset($content['group_details']['field_credits'][2])){ ?>
+				<img alt="<?php print(render($content['group_details']['field_credits'][2]))?>"
+						 src="/sites/all/themes/wwwevergreen/images/icons/catalog/credits-<?php print(render($content['group_details']['field_credits'][2]))?>.svg"/>
+			<?php } ?>
+			<? if(isset($content['group_details']['field_credits'][3])){ ?>
+				<img alt="<?php print(render($content['group_details']['field_credits'][3]))?>"
+						 src="/sites/all/themes/wwwevergreen/images/icons/catalog/credits-<?php print(render($content['group_details']['field_credits'][2]))?>.svg"/>
+			<?php }?>
 
+
+		<?php } ?>
 		<?php 		// adding the variable credit V if we already havent (credit = 0)
 		if(isset($content['field_variable_credit_options'][0]) and (render($content['group_details']['field_credits'][0]) != '0')) { ?>
 			<img alt=""
