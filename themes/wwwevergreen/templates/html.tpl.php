77373bd1247694310a56edcd0bcd6a707bbeb749
Justin McDowell <mcdowelj@evergreen.edu>
2018-02-20 13:59:07 -0800

    Moves Apple touch icon into PNG section.

--- themes/wwwevergreen/templates/html.tpl.php
+++ themes/wwwevergreen/templates/html.tpl.php
@@ -103,13 +103,14 @@ Right now I think the only difference from the default html.tpl.php is the inclu
   <meta content="Evergreen" name="application-name" />
 	<meta content="Evergreen" name="apple-mobile-web-app-title" />
 	
-	<link href="<?php print base_path() . path_to_theme() ?>/images/favicons/site-icon.png" rel="apple-touch-icon"/><!-- Homescreen icon -->
 	<!-- SVG -->
 	<link href="<?php print base_path() . path_to_theme() ?>/images/favicons/favicon.svg" rel="icon" type="image/svg+xml"/><!-- Firefox -->
 	<link color="#64933a" href="<?php print base_path() . path_to_theme() ?>/images/favicons/apple-pinned-tab.svg" rel="mask-icon"/><!-- Safari 9+ -->
 	<meta content="#64933a" name="msapplication-TileColor"/><!-- Windows 8, IE10 -->
 	<meta content="<?php print base_path() . path_to_theme() ?>/images/favicons/mstile-144x144.png" name="msapplication-TileImage"/><!-- Windows 8, IE10 -->
 	<link href="<?php print base_path() . path_to_theme() ?>/images/favicons/favicon.ico" rel="shortcut icon" type="image/x-icon"/><!-- IE and hi-dpi favicon -->
+	<!-- PNG -->
+	<link href="<?php print base_path() . path_to_theme() ?>/images/favicons/site-icon.png" rel="apple-touch-icon" sizes="276x276"/><!-- Homescreen icon, iOS, Android -->
 	<link rel="manifest" href="<?php print base_path() . path_to_theme() ?>/images/favicons/site.webmanifest" type="application/manifest+json"/><!-- Android Chrome -->
 	<!-- favicons for everybody else -->
 	<link href="<?php print base_path() . path_to_theme() ?>/images/favicons/favicon-16.png" rel="icon" sizes="16x16" type="image/png"/>
