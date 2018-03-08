cb0d0a8ee7e0cfe78c6c675c8f640689bbfd711f
Elaine Nelson <nelsone@evergreen.edu>
2018-03-08 13:32:09 -0800

    Moar debug

--- themes/wwwevergreen/templates/catalog/node--catalog_entry.tpl.php
+++ themes/wwwevergreen/templates/catalog/node--catalog_entry.tpl.php
@@ -462,7 +462,7 @@ if (!$page){ ?>
 	 						printEach($content['group_details']['group_more']['field_online_learning']);
 	 					}*/ ?>
 	 			<?php 
-		 			if(count($content['group_details']['group_more']['field_online_learning']) == 1) { print "just one."; };
+		 			print("<h2>" . count($content['group_details']['group_more']['field_online_learning']) . "</h2>");
 		 		print(render($content['group_details']['group_more']['field_online_learning']));	
 		 			
 		 		}; // end check for existence of online learning field
