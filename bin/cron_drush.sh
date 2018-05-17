7cdfa0bcceb7f63a3b35de49385ecff150c73ce8
Erik Ordway <ordwaye@evergreen.edu>
2018-05-17 13:46:05 -0700

    Update variable name used in deployment automation with the non deprecated version.
    After testing it this should be cherry picked into the test and master branches
    
    per https://docs.gitlab.com/ee/ci/variables/#9-0-renaming
    the names of certain environmental variables in gitlab CI have changed
    this is a one for one change to update to the new pattern.
    
    
    (cherry picked from commit e01a930397ce1c12a86dbb5e2472d6570f5d149d)

--- bin/cron_drush.sh
+++ bin/cron_drush.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-export CI_BUILD_REF_NAME="$1"
-if [[ -z "$CI_BUILD_REF_NAME" ]] ; then
-  export CI_BUILD_REF_NAME="dev"
+export CI_COMMIT_REF_NAME="$1"
+if [[ -z "$CI_COMMIT_REF_NAME" ]] ; then
+  export CI_COMMIT_REF_NAME="dev"
 fi
 . $HOME/bin/www_build_environment.sh
 cd $WWW_CODE
-echo "Running cron for $WWW_CODE: $CI_BUILD_REF_NAME : $1"
+echo "Running cron for $WWW_CODE: $CI_COMMIT_REF_NAME : $1"
 drush --yes cron
